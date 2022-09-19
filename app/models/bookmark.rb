71ec26144cb5c4fbeacfb2c6e44cb180f7cd3766
tomas vargas <tomasvm01@gmail.com>
2022-09-18 21:47:47 -0500

    Validations and Associations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,9 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates_associated :movie
+  validates_associated :list
+  validates :movie, uniqueness: { scope: :list }
+  validates :comment, length: { minimum: 6 }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+
+  validates :title, :overview, uniqueness: { case_sensitive: false }
+  validates :title, :overview, presence: true
+
 end
